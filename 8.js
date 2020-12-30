1e4dfe85160b44c6c345cd6c5a7e42f498d09a29
Olejjka413 <55927593+Olejjka413@users.noreply.github.com>
2020-12-30 20:42:29 +0300

    1 задание

---
+++ 8.js
@@ -0,0 +1,10 @@
+/// Задание №1 ///
+console.log('Задание №1');
+let s = prompt('Введите целое число овец:');
+let txt = '';
+for (let i = 1; i <= s; i++)
+{
+	txt = txt + `${i} овечка... `;
+}
+console.log(txt)
+
