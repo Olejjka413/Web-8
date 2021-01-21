b8d9bf2a84582a20ce7eb17b8451ddd7cab39f13
Olejjka413 <55927593+Olejjka413@users.noreply.github.com>
2021-01-21 13:48:41 +0300

    исправление

--- 8.js
+++ 8.js
@@ -45,19 +45,19 @@ let n3 = prompt('Введите третье число');
 if (n1 <= n2 && n2 <= n3) {
     console.log(n1, n2, n3)
 }
-if (n1 <= n3 && n3 <= n2) {
+else if (n1 <= n3 && n3 <= n2) {
     console.log(n1, n3, n2)
 }
-if (n2 <= n1 && n1 <= n3) {
+else if (n2 <= n1 && n1 <= n3) {
     console.log(n2, n1, n3)
 }  
-if (n2 <= n3 && n3 <= n1) {
+else if (n2 <= n3 && n3 <= n1) {
     console.log(n2, n3, n1)
 }
-if (n3 <= n1 && n1 <= n2) {
+else if (n3 <= n1 && n1 <= n2) {
     console.log(n3, n1, n2)
 }
-if (n3 <= n2 && n2 <= n1) {
+else {
     console.log(n3, n2, n1)
 }   
 
